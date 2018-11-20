736853705c74f801044d454249f74e0cbb367d10
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-11-20 15:13:17 +0100

    Enonce Ex

--- Langage/04-callback-sync.js
+++ Langage/04-callback-sync.js
@@ -1,7 +1,21 @@
 const nbs = [2, 3, 4];
 
-nbs.forEach((nb, i) => {
-  console.log(nb);
-});
+console.log(Array.isArray(nbs)) // true
+
+nbs
+  .filter((nb) => nb % 2 === 0)
+  .map((nb) => nb ** 2)
+  .forEach((nb, i) => {
+    console.log(nb);
+  });
 
 console.log('fin');
+
+
+// pile d'appel
+// ^
+// |                   log log
+// |cb cb cb   cb cb   cb  cb
+// |filter   - map   - forEach - log
+// +-----------------------------------------------------> temps
+// sortie :            4   16    Fin
---
+++ Langage/05-callback-async.js
@@ -0,0 +1,44 @@
+setTimeout(() => {
+  console.log('A')
+}, 500);
+
+setTimeout(() => {
+  console.log('B')
+}, 1000);
+
+setTimeout(() => {
+  console.log('C')
+}, 0);
+
+setTimeout(() => {
+  console.log('D')
+}, 500);
+
+/*
+setTimeout(() => {
+  console.log('E')
+}, Math.random() * 1000);
+
+setTimeout(() => {
+  console.log('F')
+}, Math.random() * 1000);
+*/
+
+console.log('Fin');
+
+// pile d'appel
+// ^
+// |
+// |                             log      log log      log
+// |st - st - st - st - log .... cbC .... cbA cbD .... cbB-*9+6
+
+// +-----------------------------2--------501-502------1001-------------> temps
+// sortie :             Fin      C        A   D        B
+
+// file d'attente (0ms) : cbC
+// file d'attente (2ms) :
+// file d'attente (500ms) : cbA cbD
+// file d'attente (501ms) : cbD
+// file d'attente (502ms) :
+// file d'attente (1000ms) : cbB
+// file d'attente (1001ms) :
